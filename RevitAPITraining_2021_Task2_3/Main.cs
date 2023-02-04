476569f6157dd6a0c7255e02e7b4493999f479d6
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-04 18:10:53 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_2021_Task2_3/Main.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_2021_Task2_3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            //List<FamilyInstance>     сolumns = new FilteredElementCollector(doc)
+            //    .OfCategory(BuiltInCategory.OST_StructuralColumns)
+            //    .WhereElementIsNotElementType()
+            //    .Cast<FamilyInstance>()
+            //    .ToList();
+
+            ElementCategoryFilter StructuralColumnsCategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralColumns);
+            ElementCategoryFilter ColumnsCategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Columns);
+            LogicalOrFilter filter = new LogicalOrFilter(StructuralColumnsCategoryFilter, ColumnsCategoryFilter);
+            List<FamilyInstance> сolumns = new FilteredElementCollector(doc)
+                .WherePasses(filter)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("Общее количество колонн в моделе:", сolumns.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
